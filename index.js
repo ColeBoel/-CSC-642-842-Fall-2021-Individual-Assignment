9cbb09ed37609ca2877b9e3fc2b92f5b62e905b5
jboelman@mail.sfsu.edu <joshcboelman@gmail.com>
2021-11-29 17:29:21 -0800

    Cookies

--- index.js
+++ index.js
@@ -1,31 +1,57 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+
+
+
+
+
+
+
+
+
 (function () {
     'use strict'
-  
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
-  
+
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
 
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
 
-
-
-  let User = {
-    firstName: '',
-    lastName: '',
-    title:'',
-
-
-  }
+(
+function storeValues(form) {
+    setCookie("lastName", form.lastName.value);
+    setCookie("field2", form.firstName.value);
+    setCookie("field3", form.title.value);
+    setCookie("field4", form.field4.value);
+    return true;
+})
+(
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";";
+})
+(
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+})
