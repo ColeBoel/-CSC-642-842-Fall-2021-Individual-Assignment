ba0ff6f74d4964e0424efd65451b7f5ff7000b99
jboelman@mail.sfsu.edu <joshcboelman@gmail.com>
2021-11-29 18:28:47 -0800

    All info stored

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 
 
 
-
-
-
-
-
-
 (function () {
     'use strict'
 
@@ -21,37 +15,9 @@
                     event.preventDefault()
                     event.stopPropagation()
                 }
-
                 form.classList.add('was-validated')
             }, false)
         })
 })()
 
-(
-function storeValues(form) {
-    setCookie("lastName", form.lastName.value);
-    setCookie("field2", form.firstName.value);
-    setCookie("field3", form.title.value);
-    setCookie("field4", form.field4.value);
-    return true;
-})
-(
-function setCookie(cname, cvalue) {
-    document.cookie = cname + "=" + cvalue + ";";
-})
-(
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-})
+
